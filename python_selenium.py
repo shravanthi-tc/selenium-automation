68caa9d64699f07ae6b8ecdd624550cf0dbae760
Shravanthi-nandeesh <shravanthi@targetclose.com>
2023-02-05 00:19:41 +0530

    updated code

--- python_selenium.py
+++ python_selenium.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("http://www.python.org")
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+d = webdriver.Chrome('/home/<user>/chromedriver',chrome_options=chrome_options)
+d.get('https://www.google.nl/')
