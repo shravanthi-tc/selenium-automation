1789a7cc424b7754ef402d999f35a7442c111d63
Shravanthi-nandeesh <shravanthi@targetclose.com>
2023-02-05 20:54:19 +0530

    updated code

--- python_selenium.py
+++ python_selenium.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-
-driver.get("https://targetclose-test.myshopify.com/admin/apps/jaya-stg")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.google.com")
