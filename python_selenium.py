b322fae9116206a455f1ee6462337d562714efda
Shravanthi-nandeesh <shravanthi@targetclose.com>
2023-02-05 20:43:37 +0530

    checking

--- python_selenium.py
+++ python_selenium.py
@@ -1,7 +1,6 @@
-# selenium 3
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
 driver.get("https://targetclose-test.myshopify.com/admin/apps/jaya-stg")
